e8e02f94145a48b63fed2c5bd7218968a1109452
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-10-16 16:21:58 -0700

    rename webpack to cjs

---
+++ webpack.config.cjs
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './client/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
