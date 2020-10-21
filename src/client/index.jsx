56ea8c9584167b4ef46ce72d9ef17c42c585aba0
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-10-21 09:58:38 -0700

    cleanup

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,23 +3,6 @@ import ReactDOM from 'react-dom'
 
 import {PlotRenderer} from './PlotRenderer'
 
-// 1589838401244,
-// 1589838461244,
-// 1589838521244,
-// 1589838581244,
-// 1589838621244,
-
-// const table = newTable(3)
-//   .addColumn('_time', 'dateTime:RFC3339', 'time', [1589838401244, 1589838461244, 1589838521244])
-//   .addColumn('_value', 'double', 'number', [2.58, 7.11, 4.79]);
-
-// const lineLayer = ;
-
-// const config = {
-//   table: table,
-//   layers: [lineLayer]
-// };
-
 ReactDOM.render(
   (<PlotRenderer />),
   document.getElementById('root')
